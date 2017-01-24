096df97088b2d544ae25f2956892c7a3d5095dfe
azaleas <azaleas@2die4.com>
2017-01-24 16:03:16 +0400

    added bold button func

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 import marked from 'marked';
 import './App.css';
 
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 const MarkdownForm = (props) =>{
   return (
     <div className="MarkdownFormWrapper">
       <h2 className="title">Markdown Editor</h2>
-      <textarea 
-        name="markdowneditor" 
-        id="markdowneditor"
-        onChange={props.onChange}
-      />
+      <div className="MarkdownForm">
+        <div className="formButtons">
+          <button value="bold"
+            onClick={props.onButtonClick}>Bold</button>
+          <button value="italic"
+            onClick={props.onButtonClick}>Italic</button>
+          <button value="olist"
+            onClick={props.onButtonClick}>Ordered List</button>
+          <button value="nlist"
+            onClick={props.onButtonClick}>Numbered List</button>
+        </div>
+        <textarea 
+          name="markdowneditor" 
+          id="markdowneditor"
+          onChange={props.onChange}
+          onMouseUp={props.onMouseUp}
+          value={props.inputValue}
+        />
+      </div>
     </div>
 
   )
@@ -36,23 +60,86 @@ class App extends Component {
     super(props);
   
     this.state = {
+      inputValue: "",
       outputValue:"",
+      startPos: "",
+      stopPos: "",
     };
   }
 
   onEditorChange = (event) => {
     let inputValue = event.target.value;
-    let inputValueEdited = marked(inputValue);
+    let eventType = event.type;
+    if(eventType === "change"){
+      this.saveEditor(inputValue);
+    }
+    if(eventType === "mouseup"){
+      let target = event.target;
+      let startPos = target.selectionStart;
+      let stopPos = target.selectionEnd;
+      if(startPos !== stopPos){
+        this.setState({
+          startPos,
+          stopPos,
+        });
+        //let selectedText = textValue.substring(startPos, stopPos);
+        //let editedValue = textValue.slice(0, startPos) + "*" + selectedText + "*" + textValue.slice(stopPos, textValue.length);
+        //this.saveEditor(editedValue);
+      }
+    }
+  }
+
+  saveEditor = (value) => {
+    let inputValueEdited = marked(value);
     this.setState({
+      inputValue: value,
       outputValue: inputValueEdited,
     });
+  }
+
+  onButtonClick = (event) => {
+    let buttonValue = event.target.value;
+    let textValue = this.state.inputValue;
+    let startPos = this.state.startPos;
+    let stopPos = this.state.stopPos;
+    if(startPos !== "" && stopPos !== ""){
+      let selectedText = textValue.substring(startPos, stopPos);
+      if(buttonValue === "bold"){
+        // Check siblings
+        let boldStart = textValue.slice(startPos - 2, startPos);
+        let boldStop = textValue.slice(stopPos, stopPos + 2);
+        let editedValue;
+        if(boldStart == "**" && boldStop == "**"){
+          editedValue = textValue.slice(0, startPos - 2) 
+          + textValue.slice(startPos, stopPos) 
+          + textValue.slice(stopPos + 2, textValue.length);
+          console.log(editedValue);
+          this.saveEditor(editedValue);
+        }
+        else{
+          editedValue = textValue.slice(0, startPos) 
+          + "**" 
+          + selectedText 
+          + "**" 
+          + textValue.slice(stopPos, textValue.length);
+          this.saveEditor(editedValue);
+        }
+        this.setState({
+          startPos: "",
+          stopPos: "",
+        });
+      }
+    }
   }
 
   render() {
     return (
       <div className="App">
         <MarkdownForm 
-          onChange={this.onEditorChange}  
+          onChange={this.onEditorChange}
+          onMouseUp={this.onEditorChange}
+          onButtonClick={this.onButtonClick} 
+          inputValue={this.state.inputValue} 
         />
         <MarkdownPreview
           editorPreview={this.state.outputValue}
